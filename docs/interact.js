651e74ed3e80c0e7b308b31cb289d2471a6119d9
Julianjmg <jjg6486@psu.edu>
2023-10-18 16:46:33 -0400

    test commit

--- docs/interact.js
+++ docs/interact.js
@@ -1,23 +1,94 @@
 window.addEventListener('DOMContentLoaded', init, false);
+const buttons = document.getElementsByTagName("button");
+let mode = "lightMode"
 
 function init() 
 {
     alert ('Hi there! Looks like the page loaded! Yay!');
-    let buttons = document.getElementsByTagName("button")
-    buttons[0].addEventListener('click', changeColor, false)
+    buttons[0].addEventListener('click', changeColor, false);
 }
 
-function changeColor() 
+function changeColor()
+{
+    
+    if (mode == "lightMode")
+    {
+        alert("IF LIGHTMODE");
+        buttons[0].addEventListener('click', darkMode, false);
+        mode = "darkMode";
+    }
+    
+    else if (mode == "darkMode")
+    {
+        alert("IF DARKMODE");
+        buttons[0].addEventListener('click', lightMode, false);
+        mode = "lightMode";
+    }
+    
+}
+
+function darkMode()
+{
+    let darken = document.getElementById("colorToggle");
+    {
+        alert("IN DARKMODE FUNCTION");
+        darken.style.backgroundColor = "black";
+        darken.style.color = "white";
+        mode = darkMode;
+        buttons[0].addEventListener('click', changeColor, false);
+    }
+}
+
+function lightMode()
+{
+    let lighten = document.getElementById("colorToggle");
+    {
+        alert("IN LIGHTMODE FUNCTION");
+        lighten.style.backgroundColor = "pink";
+        lighten.style.color = "black";
+        mode = lightMode;
+        buttons[0].addEventListener('click', changeColor, false);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function changeColor() 
 {
      var colorMe1 = document.getElementById("colorToggle") 
      {
         colorMe1.style.backgroundColor = "black";
         colorMe1.style.color = "white";
      }
-}
+}*/
 
-/*function changeColor(bool) 
+
+/*function changeColor()
 {
+    let bool = true;
     if (bool == false)
     {
         var colorMe1 = document.getElementById("colorToggle") 
@@ -27,8 +98,16 @@ function changeColor()
             bool = true;
         }
     }
-    else if (bool == true)
+    else
     {
+    buttons[0].addEventListener('click', colorChange, false)
+    console.log('Is this ELSE happening?')
+    }
+}
+function colorChange() {
+        {
+        alert("HEYYYYY");
+        console.log("HIIIIIII");
         var colorMe1 = document.getElementById("colorToggle") 
         {
             colorMe1.style.backgroundColor = "pink";
@@ -36,5 +115,6 @@ function changeColor()
             bool = false;
         }
     }
-} */
+}
+*/
 
